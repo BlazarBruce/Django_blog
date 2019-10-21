19792a0eaaa8c3f57eaa985b7e6799c02c9f94c6
zhu.chaoqiang <zhu.chaoqiang@byd.com>
2019-10-21 17:00:55 +0800

    使用模板语言编辑html文件

--- Django_cbv/blog/views.py
+++ Django_cbv/blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post, Tag
 
 def post_list(request, category_id=None, tag_id=None):
+    if tag_id:
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    else:
+        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+        if category_id:
+            post_list = post_list.filter(category_id=category_id)
 
-    return render(request, 'blog/list.html', context={'name': 'post_list'})
+    return render(request, 'blog/list.html', context={'post_list': post_list})
 
 def post_detail(request, post_id):
-
-    return render(request, 'blog/detail.html', context={'name': 'post_detail'})
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post = None
+    return render(request, 'blog/detail.html', context={'post': post})
