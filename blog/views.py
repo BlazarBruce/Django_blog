6293421ff7dfcfa1b8f93be39ded945ccefd2c04
zhu.chaoqiang <zhu.chaoqiang@byd.com>
2019-10-22 10:47:03 +0800

    虚化post_list 代码逻辑

--- blog/views.py
+++ blog/views.py
@@ -6,22 +6,30 @@ def post_list(request, category_id=None, tag_id=None):
     tag = None
     category = None
 
+    # if tag_id:
+    #     try:
+    #         tag = Tag.objects.get(id=tag_id)
+    #     except Tag.DoesNotExist:
+    #         post_list = []
+    #     else:
+    #         post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    # else:
+    #     post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+    #     if category_id:
+    #         try:
+    #             category = Category.objects.get(id=category_id)
+    #         except Category.DoesNotExist:
+    #             category = None
+    #         else:
+    #             post_list = post_list.filter(category_id=category_id)
+
     if tag_id:
-        try:
-            tag = Tag.objects.get(id=tag_id)
-        except Tag.DoesNotExist:
-            post_list = []
-        else:
-            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+        post_list, tag = Post.get_by_tag(tag_id)
+    elif category_id:
+        post_list, category = Post.get_by_category(category_id)
     else:
-        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
-        if category_id:
-            try:
-                category = Category.objects.get(id=category_id)
-            except Category.DoesNotExist:
-                category = None
-            else:
-                post_list = post_list.filter(category_id=category_id)
+        post_list = Post.latest_posts()
+
     context = {
         'category': category,
         'tag':tag,
