472881bb1a3ef237862afd2890598ea3a8c84300
zhu.chaoqiang <zhu.chaoqiang@byd.com>
2019-10-23 11:34:36 +0800

    实现Django CBV的逻辑 编写分页逻辑。

--- Django_cbv/urls.py
+++ Django_cbv/urls.py
@@ -18,15 +18,15 @@ from django.contrib import admin
 from django.conf.urls import url
 from .custom_site import custom_site
 
-from blog.views import post_list, post_detail
+from blog.views import post_list, post_detail, IndexView, CategoryView, TagView, PostDetailView
 from config.views import Links
 
 urlpatterns = [
     # 路由配置及参数传递均没有问题
-    url(r'^$', post_list, name='index'),
-    url(r'^category/(?P<category_id>\d+)/$', post_list, name='category-id'),
-    url(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-id'),
-    url(r'^post/(?P<post_id>\d+).html$', post_detail, name='post-detail'),
+    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-id'),
+    url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-id'),
+    url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
     url(r'^links/$', Links, name='links'),
     url(r'^super_admin/', admin.site.urls, name='super-admin'),  # 对应一个site (urt与path相比、url支持正则）用来管理用户
     url(r'^admin/', custom_site.urls, name='admin'),  # 对应一个site (urt与path相比、url支持正则）用来管理业务
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post, Tag, Category
 from config.models import SideBar
+from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404
 
 def post_list(request, category_id=None, tag_id=None):
     tag = None
@@ -32,3 +34,59 @@ def post_detail(request, post_id):
     }
     context.update(Category.get_navs())
     return render(request, 'blog/detail.html', context=context)
+
+class CommonViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'sidebars': SideBar.get_all(),
+        })
+        context.update(Category.get_navs())
+        return context
+
+
+# 基于CBV的实现
+class IndexView(CommonViewMixin, ListView):
+    queryset = Post.latest_posts()
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list1.html'
+
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({
+            'category': category,
+        })
+        return context
+
+    def get_queryset(self):
+        """ 重写querset，根据分类过滤 """
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({
+            'tag': tag,
+        })
+        return context
+
+    def get_queryset(self):
+        """ 重写querset，根据标签过滤 """
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag__id=tag_id)
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
