ae39493214ce7c0ff7443c766a285c24c0644bf4
zhu.chaoqiang <zhu.chaoqiang@byd.com>
2019-10-23 09:19:46 +0800

    添加侧边栏展示。

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post, Tag, Category
+from config.models import SideBar
 
 def post_list(request, category_id=None, tag_id=None):
     tag = None
@@ -16,6 +17,7 @@ def post_list(request, category_id=None, tag_id=None):
         'category': category,
         'tag':tag,
         'post_list':post_list,
+        'sidebars': SideBar.get_all(),
     }
     context.update(Category.get_navs())
     return render(request, 'blog/list.html', context=context)
@@ -26,7 +28,8 @@ def post_detail(request, post_id):
     except Post.DoesNotExist:
         post = None
     context = {
-        'post': post
+        'post': post,
+        'sidebars': SideBar.get_all(),
     }
     context.update(Category.get_navs())
     return render(request, 'blog/detail.html', context=context)
--- config/models.py
+++ config/models.py
@@ -55,5 +55,9 @@ class SideBar(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = "侧边栏"
 
+    @classmethod
+    def get_all(cls):
+        return cls.objects.filter(status=cls.STATUS_SHOW)
+
 
 
