519b0aab357e9c807fb011b519e89a42c2afd953
zhu.chaoqiang <zhu.chaoqiang@byd.com>
2019-10-24 12:12:12 +0800

    删除多于的APP

--- Django_cbv/settings/base.py
+++ Django_cbv/settings/base.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
     'blog.apps.BlogConfig',
     'config.apps.ConfigConfig',
     'comment.apps.CommentConfig',
